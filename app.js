7bc8425f2214b60df4404e45ea8033a4bd7898e8
思尘 <ward_1992@msn.cn>
2020-06-23 00:46:22 +0800

    updaet：init index

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.loadFontFace({
+      global: true,
+      family: 'TChinese',
+      source: 'url("https://colors.ichuantong.cn/font/chinese.font.woff")',
+      success: console.log
+    })
   },
   globalData: {
   }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,37 @@
 //index.js
 //获取应用实例
 // const app = getApp()
+const colors = require('../../data/colors')
+const jinrishici = require('../../utils/jinrishici.js')
 
 Page({
   data: {
+    currentColorSet: {},
+    currentColor:{},
+    poetry: {},
+    topHeight: 0
   },
   onLoad: function () {
+    const { bottom } = wx.getMenuButtonBoundingClientRect()
+    this.setData({
+      topHeight: bottom,
+      currentColorSet: colors[2],
+      currentColor: colors[2].colors[0]
+    })
+    this._fetchPoetry()
+  },
+  _fetchPoetry: function () {
+    jinrishici.load(result => {
+      let obj = {
+        content: result.data.content,
+        author: result.data.origin.author,
+        title: result.data.origin.title
+      };
+      obj.content = obj.content
+        .replace(/[，|。|！|？|、]/g, ' ')
+        .trim()
+        .split(' ');
+      this.setData({poetry: obj})
+    })
   }
 })
