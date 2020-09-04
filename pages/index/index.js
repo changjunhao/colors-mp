bcb5fa7831d54227123038a86e477846ad0ce728
思尘 <ward_1992@msn.cn>
2020-09-04 15:19:59 +0800

    U: color-view end | A: getCorrectTextColor

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,26 +3,39 @@
 // const app = getApp()
 const COLORS = require('../../data/colors')
 const jinrishici = require('../../utils/jinrishici.js')
+const { getCorrectTextColor } = require('../../utils/util')
 
 Page({
   data: {
     currentColorSet: {},
     currentColor:{},
     poetry: {},
-    topHeight: 0
+    topHeight: 0,
+    figureHeight: 'auto'
   },
   onLoad: function () {
+    this._fetchPoetry()
+    this.initState()
+  },
+  initState () {
     const { bottom } = wx.getMenuButtonBoundingClientRect()
+    const currentColorSet = COLORS[2]
+    const currentColor = currentColorSet.colors[0]
     this.setData({
       topHeight: bottom,
-      currentColorSet: COLORS[2],
-      currentColor: COLORS[2].colors[1]
-    }, () => {
-      wx.setBackgroundColor({
-        backgroundColor: this.data.currentColor.hex,
-      })
+      currentColorSet,
+      currentColor,
+      oppositeColor: getCorrectTextColor(currentColor.RGB)
     })
-    this._fetchPoetry()
+    wx.setBackgroundColor({
+      backgroundColor: currentColor.hex,
+    })
+  },
+  handleFigureLoad: function (event) {
+    const { height, width } = event.detail
+    this.setData({
+      figureHeight: `${162 * height / width}rpx`
+    })
   },
   _fetchPoetry: function () {
     jinrishici.load(result => {
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,17 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+export function getCorrectTextColor(rgb = [0, 0, 0]) {
+  /*
+  From this W3C document: http://www.webmasterworld.com/r.cgi?f=88&d=9769&url=http://www.w3.org/TR/AERT#color-contrast
+  Color brightness is determined by the following formula:
+  ((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
+  I know this could be more compact, but I think this is easier to read/explain.
+  */
+  const threshold = 130; /* about half of 256. Lower threshold equals more dark text on dark background  */
+  const [hRed, hGreen, hBlue] = rgb;
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
-module.exports = {
-  formatTime: formatTime
+  const cBrightness = (hRed * 299 + hGreen * 587 + hBlue * 114) / 1000;
+  if (cBrightness > threshold) {
+    return '#50616d';
+  } else {
+    return '#e9f1f6';
+  }
 }
